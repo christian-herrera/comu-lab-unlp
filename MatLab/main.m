e5a83a7e6428b1fb956ac07da20326b8bc084350
christian-herrera <chris1496.rp@gmail.com>
2024-12-31 00:14:08 -0300

    fix: Se corrige el main.m y se agregan detalles al Informe
    Se corrige la funcion que determina la DEP. Ademas se agrega la seccion Conclusiones al informe y la bibliografia de la cátedra.

--- MatLab/main.m
+++ MatLab/main.m
@@ -17,22 +17,22 @@ N2 = 5;
 
 
 %% Plots
-plotDEP(x, fs, 8e-5, 100, 0);
+plotDEP(x, fs, -0.5e7, 11e7, 100, 0);
 title("DEP de x[n]");
 
-plotDEP(y_B1, fs, 8e-5, 100, 0);
+plotDEP(y_B1, fs, -0.5e7, 11e7, 100, 0);
 title("DEP de x[n] Filtrada");
 
-plotDEP(y_N1, fs/N1, 0, 100, 1);
+plotDEP(y_N1, fs/N1, -1e5, 13e5, 100, 0);
 title("DEP antes del Discriminador");
 
-plotDEP(z_dis, fs/N1, 0, 1, 1);
+plotDEP(z_dis, fs/N1, 10, 80, 10, 1);
 title("DEP del Mensaje");
 
-plotDEP(z_B2, fs/N1, 0, 1, 1);
+plotDEP(z_B2, fs/N1, -10, 90, 1, 1);
 title("DEP del Mensaje Monoaural");
 
-plotDEP(z_out, fs/N1/N2, 0, 1, 1);
+plotDEP(z_out, fs/N1/N2, -10, 70, 1, 1);
 title("DEP del Mensaje con fs=45kHz");
 
 % sound(z_out, fs/N1/N2);
@@ -45,12 +45,12 @@ title("DEP del Mensaje con fs=45kHz");
 % max_val       es simplemente para fijar un limite superior con `xlim`.
 % down_sample   es para evitar sobrecargar la grafica. Elimina muestras
 % db            es para mostrar en decibeles (db = 1 anula max_val).
-function plotDEP(x, fs, max_val, down_sample, db)
-    % Obtengo la DEP de x[n] que ingresa como parametro. Junto a su `fs`
-    N = length(x);
-    X = fftshift(fft(x));
-    X = (1/(fs*N))*(abs(X).^2);
+function plotDEP(x, fs, min_val, max_val, down_sample, db)
+    % Obtengo la DEP de x[n] que ingresa como parametro. Junto a su `fs`.
+    % Esta DEP la realizo siempre con 200 realizaciones.
+    [X, N] = getDEP(fs, 200, x);
     
+    X = fftshift(X);
     if mod(N, 2) == 0
         f = (-1/2 : 1/N : (N-1)/(2*N)) * fs;
     else
@@ -74,16 +74,29 @@ function plotDEP(x, fs, max_val, down_sample, db)
 
     fig1 = figure('Position', [100 100 1000 400], 'Name', 'Laboratorio - Fund. de las Comunicaciones', 'NumberTitle', 'off');
     ax1 = axes(fig1);
-    plot(f, X);
+    plot(f, X, "LineWidth", 1.5);
     
-    if max_val ~= 0 && ~db
-        set(ax1, "Position", [0.06 0.15 0.9 0.75], "XMinorGrid", "On", "YMinorGrid", "On", "XLim", [f(1), f(end)], "YLim", [min(X), max_val]);
+    % if max_val ~= 0 && ~db
+    if max_val ~= 0
+        set(ax1, "Position", [0.06 0.15 0.9 0.75], "XMinorGrid", "On", "YMinorGrid", "On", "XLim", [f(1), f(end)], "YLim", [min_val, max_val]);
     else
         set(ax1, "Position", [0.06 0.15 0.9 0.75], "XMinorGrid", "On", "YMinorGrid", "On", "XLim", [f(1), f(end)]);
     end    
     xlabel(ax1, 'Frecuencia [Hz]');
 end
 
+
+% Esta funcion permite obtener la DEP de un proceso, dividiendo el proceso
+% completo en M realizaciones de N muestras cada uno. El proceso es x[n]
+% con una frecuencia de muestreo asociada de fs.
+function [Sxx, N] = getDEP(fs, M, x)
+    N = floor(length(x)/M);
+    Sxx = zeros(N, 1);
+    for m=0:M-1
+        TDF = fft(x(m*N+1:N*(m+1)));
+        Sxx = Sxx + (fs/(M*N))*abs(TDF).^2;
+    end
+end
 
 
 
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 <h2 align="center">Laboratorio Nro. 1</h2><br/>
 <div align="center">
 
-![Static Badge](https://img.shields.io/badge/Version-1.1-1565C0?style=for-the-badge)
+![Static Badge](https://img.shields.io/badge/Version-1.2-1565C0?style=for-the-badge)
 ![GitHub License](https://img.shields.io/github/license/christian-herrera/esp12-leme?style=for-the-badge)
 
 </div>
